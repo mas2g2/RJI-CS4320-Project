019a5ea9fb64203fc5238b40145fd34ab1d71479
mas2g2 <mas2g2@mail.missouri.edi>
2019-04-12 14:38:06 -0500

    unwanted files or some inline comments

--- user.sql
+++
@@ -1,9 +0,0 @@
-create database userstable;
-use userstable;
-
-create table users(
-	Username varchar(255),
-	Email varchar(255),
-	Password varchar(255),
-	primary key(Username,Email)
-);
--- weighttracker.sql
+++
@@ -1,9 +0,0 @@
-create database weight_tracker;
-use weight_tracker;
-
-create table users(
-	Username varchar(255),
-	Email varchar(255),
-	Password varchar(255),
-	primary key(Username,Email)
-);
