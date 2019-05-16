58567ebb8e358f26c3a822836debc2057cf6a8ba
JRunner97 <jthompson706.jt@gmail.com>
2019-05-16 21:11:12 +0000

    final push

--- app.py
+++ app.py
@@ -3,15 +3,36 @@ import mysql.connector
 import subprocess
 import piexif
 import os
+import re
 import json
 import time
+import exifread
 from PIL import Image
 from werkzeug.utils import secure_filename
+from flask_mysqldb import MySQL
+import PIL.ExifTags
 app = Flask(__name__)
+# app.config.from_envvar('YOURAPPLICATION_SETTINGS')
+# app.config.from_pyfile("/home/ubuntu/init.py", silent=True)
 
 UPLOAD_FOLDER = '/home/ubuntu/static/img/tmp/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+f = open("init.txt", "r")
+host = f.readline().rstrip()
+user = f.readline().rstrip()
+password = f.readline().rstrip()
+db = f.readline().rstrip()
+f.close()
+
+
+app.config['MYSQL_HOST'] = host
+app.config['MYSQL_USER'] = user
+app.config['MYSQL_PASSWORD'] = password
+app.config['MYSQL_DB'] = db
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -19,9 +40,14 @@ def home():
     if user_id:
         return render_template('home.html')
     else:
-        return render_template("login.html") 
+        return redirect(url_for('login'))
     
     
+@app.route('/aboutthissite/')
+def aboutthissite():
+    return render_template('aboutthissite.html')
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -32,21 +58,32 @@ def login_page():
 
     try: 
         # TODO set up read_db_config
-        mydb = mysql.connector.connect(
-          host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
-          user="adminUser",
-          passwd="password123",
-          database="rjiDB"
-        )
-        mycursor = mydb.cursor()
+        # mydb = mysql.connector.connect(
+        #   host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
+        #   user="adminUser",
+        #   passwd="password123",
+        #   database="rjiDB"
+        # )
+        # mycursor = mydb.cursor()
+        mycursor = mysql.connection.cursor()
 	       
         if request.method == "POST":
+            validString = re.compile("^[A-Za-z0-9]*$")
             
             # handle login request
             if request.form['action'] == "loginMeIn":
                 
                 attempted_username = request.form['username']
                 attempted_password = request.form['password']
+
+                if not validString.match(attempted_password):
+                    error = 'Invalid password. Please try again!'
+                    return render_template('login.html', error = error)
+
+                if not validString.match(attempted_username):
+                    error = 'Invalid username. Please try again!'
+                    return render_template('login.html', error = error)
+
                 
                 userCredentialsQuery = "SELECT * FROM User WHERE username='"+ attempted_username +"' AND password='"+ attempted_password +"'"
                 
@@ -60,9 +97,7 @@ def login_page():
                     return render_template('login.html', error = error)
                     
                 else:
-                    
-                    response = redirect("http://3.14.73.158")
-                    print(userCredentialsQueryResult)
+                    response = redirect("/")
                     response.set_cookie('loggedInUser', str(userCredentialsQueryResult[0]))
                     
                     return response
@@ -70,12 +105,30 @@ def login_page():
                 
             # handle register user request
             elif request.form['action'] == "signMeUp":
+
+                validString = re.compile("^[A-Za-z0-9']*$")
                 
                 attempted_first_name = request.form['first_name']
                 attempted_last_name = request.form['last_name']
                 attempted_email = request.form['email']
                 attempted_username = request.form['username']
                 attempted_password = request.form['password']
+
+                if not validString.match(attempted_first_name):
+                    error = 'Invalid characters in first name. Please try again!'
+                    return render_template('login.html', error = error)
+                
+                if not validString.match(attempted_last_name):
+                    error = 'Invalid characters in last name. Please try again!'
+                    return render_template('login.html', error = error)
+                
+                if not validString.match(attempted_username):
+                    error = 'Invalid username. Please try again!'
+                    return render_template('login.html', error = error)
+                
+                if not validString.match(attempted_password):
+                    error = 'Invalid password. Please try again!'
+                    return render_template('login.html', error = error)
 		          
                 # check for invalid username
                 # TODO do regex check
@@ -101,27 +154,27 @@ def login_page():
                     sql = "INSERT INTO User (username, password, email, first_name, last_name) VALUES (%s, %s, %s, %s, %s)"
                     val = (attempted_username, attempted_password, attempted_email, attempted_first_name, attempted_last_name)
                     mycursor.execute(sql, val)
-                    mydb.commit()
+                    mysql.connection.commit()
                     
                     userCredentialsQuery = "SELECT * FROM User WHERE username='"+ attempted_username +"' AND password='"+ attempted_password +"'"
                     mycursor.execute(userCredentialsQuery)
                     userCredentialsQueryResult = mycursor.fetchone()
                     
                 except Exception as e:
-                    print(e);
+                    print(e)
                     error = 'Failed to create account'
                     return render_template('login.html', error = error)
                 
                 # if successful, then redirect to homepage
-                response = redirect("http://3.14.73.158")
-                response.set_cookie('loggedInUser', userCredentialsQuery[0])
+                response = redirect("/")
+                response.set_cookie('loggedInUser', userCredentialsQueryResult[0])
                 return response
 
-        return redirect("http://3.14.73.158")
+        return redirect(url_for('login'))
 
     except Exception as e:
 
-        response = redirect("http://3.14.73.158")
+        response = redirect(url_for('login'))
         response.set_cookie('exception', e)
 
         return response
@@ -148,18 +201,19 @@ def rating_photos():
     # TODO set interval action that will check how old a photo is and delete old ones
     try:
         # TODO set up read_db_config
-        mydb = mysql.connector.connect(
-          host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
-          user="adminUser",
-          passwd="password123",
-          database="rjiDB"
-        )
-        mycursor = mydb.cursor()
+        # mydb = mysql.connector.connect(
+        #   host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
+        #   user="adminUser",
+        #   passwd="password123",
+        #   database="rjiDB"
+        # )
+        # mycursor = mydb.cursor()
+        mycursor = mysql.connection.cursor()
         
         user_id = request.cookies.get('loggedInUser')
         if user_id:
             if request.method == "POST":
-                
+
                 # delete user's previous photos from server
                 userPhotosQuery = "SELECT * FROM Photo WHERE user_id="+ user_id
                 mycursor.execute(userPhotosQuery)
@@ -171,7 +225,7 @@ def rating_photos():
                 # delete all photo records previously in the database for this user
                 sql = "DELETE FROM Photo WHERE user_id = " + user_id
                 mycursor.execute(sql)
-                mydb.commit()
+                mysql.connection.commit()
 
                 # check if the post request has the file part
                 if 'file[]' not in request.files:
@@ -194,13 +248,14 @@ def rating_photos():
                             sql = "INSERT INTO Photo (filepath, user_id) VALUES (%s, %s)"
                             val = (absoluteFilepath, user_id)
                             mycursor.execute(sql, val)
-                            mydb.commit()
+                            mysql.connection.commit()
                             filePathList.append(absoluteFilepath)
 
                         except Exception as e:
-                            print(e);
+                            print(e)
                             error = 'Unable to Upload Photos'
-                            return render_template('home.html', error = error)
+                            return redirect(url_for('/'), error=error)
+                            #return render_template('home.html', error = error)
 
                 uploaded_directory = request.files.getlist("directory[]")
                 if uploaded_directory is None:
@@ -215,13 +270,14 @@ def rating_photos():
                             sql = "INSERT INTO Photo (filepath, user_id) VALUES (%s, %s)"
                             val = (absoluteFilepath, user_id)
                             mycursor.execute(sql, val)
-                            mydb.commit()
+                            mysql.connection.commit()
                             filePathList.append(absoluteFilepath)
 
                         except Exception as e:
-                            print(e);
+                            print(e)
                             error = 'Unable to Upload Photos'
-                            return render_template('home.html', error = error)
+                            return redirect(url_for('/'), error=error)
+                            # return render_template('home.html', error = error)
                         
                 # convert all files to lower-case .jpg
                 #
@@ -229,8 +285,6 @@ def rating_photos():
                 mycursor.execute(userPhotosQuery)
                 userPhotosQueryResults = mycursor.fetchall()  
 
-                
-                
                 for photo in userPhotosQueryResults:
                     originalFileName = photo[1]
                     if originalFileName[-3:] == "JPG":
@@ -247,9 +301,21 @@ def rating_photos():
                         # update record in database to jpg
                         sql = "UPDATE Photo SET filepath = '" + filename + "' WHERE filepath LIKE '" + photo[1] + "'"
                         mycursor.execute(sql)
-                        mydb.commit()
+                        mysql.connection.commit()
                     elif originalFileName != "jpg":
                         pass
+
+                userPhotosQuery = "SELECT * FROM Photo WHERE user_id="+ user_id
+                mycursor.execute(userPhotosQuery)
+                userPhotosQueryResults = mycursor.fetchall()  
+
+                for photo in userPhotosQueryResults:
+                    referencePath = photo[1].replace('/home/ubuntu/','')
+                    print(referencePath)
+                    f = open(referencePath, 'rb')
+                    # Return Exif tags
+                    tags = exifread.process_file(f)
+                    print(tags)
         
                 userPhotosQuery = "SELECT * FROM Photo WHERE user_id="+ user_id
                 mycursor.execute(userPhotosQuery)
@@ -289,13 +355,14 @@ def photo_gallery():
     
     try:
         # TODO set up read_db_config
-        mydb = mysql.connector.connect(
-          host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
-          user="adminUser",
-          passwd="password123",
-          database="rjiDB"
-        )
-        mycursor = mydb.cursor()
+        # mydb = mysql.connector.connect(
+        #   host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
+        #   user="adminUser",
+        #   passwd="password123",
+        #   database="rjiDB"
+        # )
+        # mycursor = mydb.cursor()
+        mycursor = mysql.connection.cursor()
         
         user_id = request.cookies.get('loggedInUser')
         if user_id: 
@@ -318,7 +385,72 @@ def photo_gallery():
 
             photoArray = [{"Rating": t, "relativePath": s} for t, s in zip(Rating, relativePath)]
 
-            userPhotos = json.dumps(photoArray)
+            userPhotos = json.loads(json.dumps(photoArray))
+
+            print(userPhotos)
+
+            for photo in userPhotos:
+                print(photo['Rating'])
+
+            # add tags
+    #        for photo in userPhotosQueryResults:
+    #            exif_dict = piexif.load(photo[1])
+    #            print(exif_dict['Exif'])
+    #            for ifd in ("0th", "Exif", "GPS", "1st"):
+    #                for tag in exif_dict[ifd]:
+    #                    print(piexif.TAGS[ifd][tag]["name"], exif_dict[ifd][tag])
+
+            return render_template("photoGallery.html", userPhotos = userPhotos, imgr = 0)
+        else:
+            return redirect("/login/") 
+    
+    except Exception as e:
+        print(e)
+        return redirect('/')
+
+    finally:
+        mycursor.close()
+
+
+@app.route('/getRating/', methods=["GET","POST"])
+def get_Rating():
+    try:
+        # if request.form['imgrating']:
+        #option = request.form['options']
+        imgr = request.form['imgrating']
+
+        # TODO set up read_db_config
+        # mydb = mysql.connector.connect(
+        # host="ec2-3-14-73-158.us-east-2.compute.amazonaws.com",
+        # user="adminUser",
+        # passwd="password123",
+        # database="rjiDB"
+        # )
+        # mycursor = mydb.cursor()
+        mycursor = mysql.connection.cursor()
+        
+        user_id = request.cookies.get('loggedInUser')
+        if user_id: 
+
+            # run each file individually, batch too slow
+            #subprocess_cmd('cd image-quality-assessment; ./predict --docker-image nima-cpu --base-model-name MobileNet --weights-file /home/ubuntu/image-quality-assessment/models/MobileNet/weights_mobilenet_aesthetic_0.07.hdf5 --image-source /tmp/images')
+            #subprocess.run("cd image-quality-assessment && pwd")
+
+            userPhotosQuery = "SELECT * FROM Photo WHERE user_id=" + user_id
+            mycursor.execute(userPhotosQuery)
+            userPhotosQueryResults = mycursor.fetchall()
+
+            relativePath, Rating = [], []
+            for photo in userPhotosQueryResults:
+                referencePath = photo[1].replace('/home/ubuntu/','')
+                # {key:value mapping}
+
+                relativePath.append(referencePath)
+                Rating.append(photo[2])
+
+            photoArray = [{"Rating": t, "relativePath": s} for t, s in zip(Rating, relativePath)]
+
+            userPhotos = json.loads(json.dumps(photoArray))
 
             print(userPhotos)
 
@@ -330,7 +462,7 @@ def photo_gallery():
     #                for tag in exif_dict[ifd]:
     #                    print(piexif.TAGS[ifd][tag]["name"], exif_dict[ifd][tag])
 
-            return render_template("photoGallery.html", userPhotos = userPhotos)
+            return render_template("photoGallery.html", userPhotos = userPhotos, imgr = float(imgr))
         else:
             return redirect("/login/") 
     
@@ -340,7 +472,6 @@ def photo_gallery():
 
     finally:
         mycursor.close()
-
 
 @app.route('/logout/')
 def logout():
